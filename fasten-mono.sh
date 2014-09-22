74b008928ada77d0a8bc12066b0aa11128fdc697
Jon Purdy <evincarofautumn@gmail.com>
2014-09-22 14:48:16 -0700

    ahcbench

--- fasten-mono.sh
+++ fasten-mono.sh
@@ -3,11 +3,12 @@ time -p \
     ( \
     /Users/jonathanpurdy/Projects/mono/mono/mini/mono-sgen \
     "$@" \
-    ~/Projects/benchmarker/tests/csolden/Health/Health/bin/Debug/Health.exe -l 10 -t 40 \
+    ~/Projects/benchmarker/tests/zorn/ahcbench/ahcbench.exe ~/Projects/benchmarker/tests/zorn/ahcbench/input3.cs \
     > output \
     2> errors \
     ) 2>timing
 
+# ~/Projects/benchmarker/tests/csolden/Health/Health/bin/Debug/Health.exe -l 10 -t 40
 # /Users/jonathanpurdy/Projects/benchmarker/tests/zorn/lcscbench/lcscbench.exe /Users/jonathanpurdy/Projects/benchmarker/tests/zorn/lcscbench/input1.cs
 
 perl -ne 'if (/real\s+([\d.]+)/) { printf "%f", $1; }' timing
