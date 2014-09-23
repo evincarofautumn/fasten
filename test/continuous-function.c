c17ba9f1af2075a7ae350af95ca0ba2c180bef88
Mark Probst <mark.probst@gmail.com>
2014-09-23 13:23:04 -0700

    Correct continuous-function.c and add a power of two.

--- test/continuous-function.c
+++ test/continuous-function.c
@@ -3,11 +3,11 @@
 
 #define A	10	/* FASTENABLE */
 #define B	3	/* FASTENABLE */
-#define C	5	/* FASTENABLE */
+#define C	8	/* FASTENABLE */
 
 int
 main (void)
 {
-	printf ("%f\n", fabs ((A-5.5)*(B-8.5)*(C*2.5)));
+	printf ("%f\n", fabs ((A-5.5)*(B-8.5)*(C-2.5)));
 	return 0;
 }
